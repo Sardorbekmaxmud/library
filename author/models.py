a2d089ad67cd710a43572f608fee47f06e068094
imhasanboy <129838299+imhasanboy@users.noreply.github.com>
2023-07-17 18:39:29 +0500

    Update models.py

--- author/models.py
+++ author/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class AuthorModel(models.Model):
+    name = models.CharField(max_length=20,default='')
+    fname = models.CharField(max_length=20,default='')
+    date_of_birth = models.DateField(default=datetime.now)
+    country = models.CharField(max_length=20,default='')
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        db_table = 'author'
