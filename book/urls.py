cc670a62e6601437e0f467cbbde7326a7b45fd37
Sardorbekmaxmud <sardorbekmaxmudov33@gmail.com>
2023-07-18 19:32:32 +0500

    push branch

--- book/urls.py
+++ book/urls.py
@@ -1,13 +1,4 @@
 from django.urls import path
-<<<<<<< HEAD
-from .views import (BookListCreatView,BookDetailUpdateDeleteView,CategoryAllBookView,AuthorBookListView)
-
-urlpatterns = [
-    path('',BookListCreatView.as_view()),
-    path('category/<int:id>/',CategoryAllBookView.as_view()),
-    path('all/author/<int:id>/',AuthorBookListView.as_view()),
-    path('<pk>/',BookDetailUpdateDeleteView.as_view()),
-=======
 from .views import (BookListCreatView,BookDetailView,BookUpdateView,BookDeleteView,
                     CategoryAllBookView,AuthorBookListView)
 
@@ -18,5 +9,4 @@ urlpatterns = [
     path('delete/<pk>/',BookDeleteView.as_view()),
     path('category/<int:id>/',CategoryAllBookView.as_view()),
     path('all/author/<int:id>/',AuthorBookListView.as_view()),
->>>>>>> d7caeb229884e831d99fd1917b634cdd7a31c8f1
 ]
