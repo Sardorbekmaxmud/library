eb494c5db02c489edbdaf09650006844f03e64dc
Sardorbekmaxmud <sardorbekmaxmudov33@gmail.com>
2023-07-18 19:37:07 +0500

    commit

--- config/permissions.py
+++ config/permissions.py
@@ -2,5 +2,4 @@ from rest_framework.permissions import BasePermission
 
 class UserPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
-        print(request.user,obj.user)
         return request.user==obj.user
